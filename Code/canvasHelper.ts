03ffc8a91f97d974cdd87542752e755f09654d00
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-02-08 11:34:38 +0530

    Added Greyscale, I hate running tests on myself :frowning:

--- Code/canvasHelper.ts
+++ Code/canvasHelper.ts
@@ -218,7 +218,7 @@ export default class CanvasHelper {
 	 * @param image The image for which you want to invert the colours
 	 * @description Inverts the colour scheme of the image provided
 	 */
-	static async invert(image: baseImage): Promise<Buffer> {
+	public static async invert(image: baseImage): Promise<Buffer> {
 		if(!this.isBuffString(image)) throw new Error("Image not provided or invalid Image type");
 
 		try {
@@ -239,6 +239,37 @@ export default class CanvasHelper {
 
 			ctx.putImageData(imgData, 0, 0);
 
+
+			return canvas.toBuffer();
+		}
+		catch (err) {
+			throw new Error(err.message);
+		}
+	}
+
+	/**
+	 * @param image
+	 */
+	public static async greyscale(image: baseImage): Promise<Buffer> {
+		if(!this.isBuffString(image)) throw new Error("Image not provided or invalid Image type");
+
+		try {
+			const base = await loadImage(image);
+			const canvas = createCanvas(base.width, base.height);
+			const ctx =canvas.getContext("2d");
+
+			ctx.drawImage(base, 0, 0);
+
+			const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+			for (let i = 0; i < imgData.data.length; i += 4) {
+				const brightness = 0.34 * imgData.data[i] + 0.5 * imgData.data[i + 1] + 0.16 * imgData.data[i + 2];
+				imgData.data[i] = brightness;
+				imgData.data[i + 1] = brightness;
+				imgData.data[i + 2] = brightness;
+			}
+
+			ctx.putImageData(imgData, 0, 0);
 
 			return canvas.toBuffer();
 		}
--- Tests/tests.ts
+++ Tests/tests.ts
@@ -3,14 +3,5 @@ import { writeFileSync } from "fs";
 
 const testURI: string = "https://cdn.discordapp.com/avatars/715196366321614908/40fe601ce2722ad7d333fbde06eba828.png?size=1024";
 
-Main.CanvasHelper.tweet(testURI, "SpiderBro", "SpiderBro", "https://github.com/SpiderMath")
-	.then((res) => writeFileSync("./Tweet.png", res));
-
-Main.CanvasHelper.invert(testURI)
-	.then((res) => writeFileSync("./invert.png", res));
-
-Main.CanvasHelper.sepia(testURI)
-	.then((res) => writeFileSync("./sepia.png", res));
-
-Main.CanvasHelper.wideImage(testURI)
-	.then((res) => writeFileSync("./wideImage.png", res));
+Main.CanvasHelper.greyscale(testURI)
+	.then(res => writeFileSync("greyscale.png", res));
